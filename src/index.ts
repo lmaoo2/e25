06a3e1f88e08aea4318e2dcf6aab4a2fd65877e8
lmaoo2 <yk2731804@gmail.com>
2025-04-09 00:14:55 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"], // Add your frontend URL
+  origin: ["*"], // Add your frontend URL
   credentials: true
 }));
 
